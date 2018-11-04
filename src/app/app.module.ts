93f3e064e17ddac91dcfa17df1f92af9ed38258e
viru9 <steelviraj@gmail.com>
2018-11-04 18:31:00 +0530

    basic task comited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
@@ -16,7 +15,6 @@ import { ServersComponent } from './servers/servers.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
     FormsModule
   ],
